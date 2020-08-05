ffd5dfb71615fc29000ffd8fc6a020fa598b755e
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-08-05 22:40:13 +0300

    Winning handled properly

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Card from "./Card";
 
 var nrRevealed = 0;
 var lookingForPair = 0;
+var foundPairs = 0;
+var nrOfPairs = 4;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -36,10 +39,13 @@ class App extends React.Component {
     }
 
     restart() {
+        console.log("Restart triggered");
         nrRevealed = 0;
         lookingForPair = 0;
+        foundPairs = 0;
         let cards = this.setupCards(); // Nya fräscha kort med alla default states
         this.setState({ cardStates: cards }); // Uppdatera state med de nya korten
+        this.setState({ solved: false }); // Gick inte att uppdatera hela state-objektet på en gång så...
     }
     cardClick(id) {
         let cardArray = this.state.cardStates; // Tillfällig array med objekt för varje korts olika värden
@@ -61,15 +67,17 @@ class App extends React.Component {
             cardArray[id].revealed = !cardArray[id].revealed; // Toggla statusen för uppvänt på rätt objekt i arrayen
             cardArray[id].revealed ? (nrRevealed += 1) : (nrRevealed -= 1); // Öka eller minska räknare på antal uppvända kort
             if (cardArray[id].pairID === lookingForPair) {
-                // Kontrollera om korten matchar
+                // Hittat ett par - korten matchar
                 cardArray.forEach((element) => {
                     // Gå igenom arrayen och markera paret som hittat
                     if (element.pairID === lookingForPair) {
                         element.found = true;
                     }
                 });
-                // TODO: Om det är 0 kort med found: false - sätt solved till true
-
+                foundPairs += 1;
+                if (foundPairs === nrOfPairs) {
+                    this.setState({ solved: true });
+                }
                 nrRevealed = 0;
             }
             this.setState({ cardStates: cardArray }); // Uppdatera states med data från den tillfälliga arrayen
@@ -79,6 +87,19 @@ class App extends React.Component {
     render() {
         return (
             <>
+                {this.state.solved && (
+                    <div className="flex">
+                        <button
+                            className="mx-auto border-2 border-orange-300 rounded-lg mt-4 p-4 bg-orange-200"
+                            onClick={() => {
+                                this.restart();
+                            }}
+                        >
+                            Nytt spel
+                        </button>
+                    </div>
+                )}
+
                 <div className="grid grid-cols-4 gap-4 mx-auto mt-4 max-w-6xl">
                     {this.state.cardStates.map((current, index) => {
                         return (
@@ -91,17 +112,6 @@ class App extends React.Component {
                             />
                         );
                     })}
-                    {this.state.solved && <div>Grattis!</div>}
-                </div>
-                <div className="flex">
-                    <button
-                        className="mx-auto border-2 border-gray p-4 bg-orange-300"
-                        onClick={() => {
-                            this.restart();
-                        }}
-                    >
-                        Börja om
-                    </button>
                 </div>
             </>
         );
