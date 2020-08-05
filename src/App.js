73f2613490137c4664f0c4cb353d59aa9c77ebbf
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-08-05 20:40:29 +0300

    Improved gui, restart button added

--- src/App.js
+++ src/App.js
@@ -5,34 +5,44 @@ var nrRevealed = 0;
 var lookingForPair = 0;
 class App extends React.Component {
     constructor(props) {
-		super(props);
-		let cards = this.setupCards();
+        super(props);
+        let cards = this.setupCards();
         this.state = {
+            solved: false,
             cardStates: cards,
         };
-		this.cardClick = this.cardClick.bind(this);
-		this.setupCards = this.setupCards.bind(this);
+        this.cardClick = this.cardClick.bind(this);
+        this.setupCards = this.setupCards.bind(this);
+        this.restart = this.restart.bind(this);
     }
-	setupCards() {
-		let rawCards = [
-			{ revealed: false, pairID: 0, found: false, value: "Flundra" },
-			{ revealed: false, pairID: 0, found: false, value: "Flundra" },
-			{ revealed: false, pairID: 1, found: false, value: "Kotte" },
-			{ revealed: false, pairID: 1, found: false, value: "Kotte" },
-			{ revealed: false, pairID: 2, found: false, value: "Pinne" },
-			{ revealed: false, pairID: 2, found: false, value: "Pinne" },
-			{ revealed: false, pairID: 3, found: false, value: "Planka" },
-			{ revealed: false, pairID: 3, found: false, value: "Planka" },
-		]
-		for(let i = rawCards.length - 1; i > 0; i--){
-			const j = Math.floor(Math.random() * i)
-			const temp = rawCards[i]
-			rawCards[i] = rawCards[j]
-			rawCards[j] = temp
-		  }
-		return rawCards
-	}
-    cardClick(id) {        
+    setupCards() {
+        let rawCards = [
+            { revealed: false, pairID: 0, found: false, value: "Flundra" },
+            { revealed: false, pairID: 0, found: false, value: "Flundra" },
+            { revealed: false, pairID: 1, found: false, value: "Kotte" },
+            { revealed: false, pairID: 1, found: false, value: "Kotte" },
+            { revealed: false, pairID: 2, found: false, value: "Pinne" },
+            { revealed: false, pairID: 2, found: false, value: "Pinne" },
+            { revealed: false, pairID: 3, found: false, value: "Planka" },
+            { revealed: false, pairID: 3, found: false, value: "Planka" },
+        ];
+        for (let i = rawCards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i);
+            const temp = rawCards[i];
+            rawCards[i] = rawCards[j];
+            rawCards[j] = temp;
+        }
+        return rawCards;
+    }
+
+    restart() {
+        console.log("Restart");
+        nrRevealed = 0;
+        lookingForPair = 0;
+        let cards = this.setupCards();
+        this.setState({ cardStates: cards });
+    }
+    cardClick(id) {
         let cardArray = this.state.cardStates; // Tillfällig array med objekt för varje korts olika värden
         if (nrRevealed === 2) {
             // Vänd ned korten vid klick om två kort redan är uppe
@@ -59,7 +69,8 @@ class App extends React.Component {
                         element.found = true;
                     }
                 });
-                
+                // TODO: Om det är 0 kort med found: false - sätt solved till true
+
                 nrRevealed = 0;
             }
             this.setState({ cardStates: cardArray }); // Uppdatera states med data från den tillfälliga arrayen
@@ -68,19 +79,32 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="grid grid-cols-4 gap-4 mx-auto mt-4 max-w-6xl">
-                {this.state.cardStates.map((current, index) => {
-                    return (
-                        <Card
-                            key={index}
-                            id={index}
-                            revealed={this.state.cardStates[index].revealed}
-                            clickHandler={this.cardClick}
-                            text={this.state.cardStates[index].value}
-                        />
-                    );
-                })}
-            </div>
+            <>
+                <div className="grid grid-cols-4 gap-4 mx-auto mt-4 max-w-6xl">
+                    {this.state.cardStates.map((current, index) => {
+                        return (
+                            <Card
+                                key={index}
+                                id={index}
+                                revealed={this.state.cardStates[index].revealed}
+                                clickHandler={this.cardClick}
+                                text={this.state.cardStates[index].value}
+                            />
+                        );
+                    })}
+                    {this.state.solved && <div>Grattis!</div>}
+                </div>
+				<div className="flex">
+					<button
+						className="mx-auto border-2 border-gray p-4 bg-orange-300"
+						onClick={() => {
+							this.restart();
+						}}
+					>
+						Börja om
+					</button>
+				</div>
+            </>
         );
     }
 }
--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,13 @@ import React from "react";
 import './Card.css'
 
 export default function Card(props) {
+
     return (
         <button
             onClick={() => {
                 return props.clickHandler(props.id);
-            }}
-            className="border-2 leading-8 text-5xl my-8"
+			}}
+            className={props.revealed ? "card--turned border-2 leading-8 md:text-5xl my-8" : "card border-2 leading-8 md:text-5xl my-8" }
         >
             {props.revealed ? props.text : ""}
         </button>
