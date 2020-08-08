ef63ecc0e32a67c6aa019c74e6236885d8b7109d
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-08-08 17:40:39 +0300

    Menu system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "./Card";
-import Deck from "./Deck"
+import Deck from "./Deck";
 
 var nrRevealed = 0;
 var lookingForPair = 0;
@@ -12,21 +12,29 @@ class App extends React.Component {
         super(props);
         let cards = Deck(nrOfPairs);
         this.state = {
+            menuOpen: true,
             solved: false,
             cardStates: cards,
         };
         this.cardClick = this.cardClick.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
     }
 
-	restart(pairCount) {
+    toggleMenu() {
+        this.setState({ menuOpen: !this.state.menuOpen });
+        return;
+    }
+
+    restart(pairCount) {
         console.log("Restart triggered");
         nrRevealed = 0;
         lookingForPair = 0;
-		foundPairs = 0;
-		nrOfPairs = pairCount
+        foundPairs = 0;
+        nrOfPairs = pairCount;
         let cards = Deck(pairCount); // Nya fräscha kort med alla default states
         this.setState({ cardStates: cards }); // Uppdatera state med de nya korten
         this.setState({ solved: false }); // Gick inte att uppdatera hela state-objektet på en gång så...
+        this.setState({ menuOpen: false });
     }
     cardClick(id) {
         let cardArray = this.state.cardStates; // Tillfällig array med objekt för varje korts olika värden
@@ -78,8 +86,48 @@ class App extends React.Component {
 
     render() {
         return (
-            <>
-                    <div className="flex justify-center">
+            <>	<div className="flex justify-end"><button
+                    onClick={this.toggleMenu}
+                    className="border-2 border-black rounded-lg m-4 p-4 bg-gray-200"
+                >
+                    {this.state.menuOpen ? (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-x"
+                        >
+                            <line x1="18" y1="6" x2="6" y2="18"></line>
+                            <line x1="6" y1="6" x2="18" y2="18"></line>
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-menu"
+                        >
+                            <line x1="3" y1="12" x2="21" y2="12"></line>
+                            <line x1="3" y1="6" x2="21" y2="6"></line>
+                            <line x1="3" y1="18" x2="21" y2="18"></line>
+                        </svg>
+                    )}
+                </button>
+				</div>
+                {this.state.menuOpen ? (
+                    <div className="flex px-4 flex-wrap justify-center">
                         <button
                             className="border-2 border-orange-300 rounded-lg mt-4 p-4 bg-orange-200"
                             onClick={() => {
@@ -88,7 +136,7 @@ class App extends React.Component {
                         >
                             Nytt spel (8 kort)
                         </button>
-						<button
+                        <button
                             className="border-2 mx-4 border-orange-300 rounded-lg mt-4 p-4 bg-orange-200"
                             onClick={() => {
                                 this.restart(8);
@@ -96,7 +144,7 @@ class App extends React.Component {
                         >
                             Nytt spel (16 kort)
                         </button>
-						<button
+                        <button
                             className="border-2 border-orange-300 rounded-lg mt-4 p-4 bg-orange-200"
                             onClick={() => {
                                 this.restart(16);
@@ -105,22 +153,25 @@ class App extends React.Component {
                             Nytt spel (32 kort)
                         </button>
                     </div>
-
-                <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 mx-auto mt-4 max-w-6xl">
-                    {this.state.cardStates.map((current, index) => {
-                        return (
-                            <Card
-                                key={index}
-								id={index}
-								pairCount={nrOfPairs}
-                                revealed={this.state.cardStates[index].revealed}
-                                clickHandler={this.cardClick}
-								text={this.state.cardStates[index].value}
-								found={this.state.cardStates[index].found}
-                            />
-                        );
-                    })}
-                </div>
+                ) : (
+                    <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 mx-auto mt-4 max-w-6xl">
+                        {this.state.cardStates.map((current, index) => {
+                            return (
+                                <Card
+                                    key={index}
+                                    id={index}
+                                    pairCount={nrOfPairs}
+                                    revealed={
+                                        this.state.cardStates[index].revealed
+                                    }
+                                    clickHandler={this.cardClick}
+                                    text={this.state.cardStates[index].value}
+                                    found={this.state.cardStates[index].found}
+                                />
+                            );
+                        })}
+                    </div>
+                )}
             </>
         );
     }
