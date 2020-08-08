fd067dfd2bb39b87bad7a95f712afb801f46173a
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-08-08 19:37:18 +0300

    Clear and readable logic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 import Card from "./Card";
 import Deck from "./Deck";
 
-var nrRevealed = 0;
-var lookingForPair = 0;
-var foundPairs = 0;
+var lookingForPair;
 var nrOfPairs = 4;
+var moveAvailableToggle = true;
 
 class App extends React.Component {
     constructor(props) {
@@ -27,105 +26,99 @@ class App extends React.Component {
 
     restart(pairCount) {
         console.log("Restart triggered");
-        nrRevealed = 0;
         lookingForPair = 0;
-        foundPairs = 0;
+        moveAvailableToggle = true;
         nrOfPairs = pairCount;
         let cards = Deck(pairCount); // Nya fräscha kort med alla default states
         this.setState({ cardStates: cards }); // Uppdatera state med de nya korten
         this.setState({ solved: false }); // Gick inte att uppdatera hela state-objektet på en gång så...
         this.setState({ menuOpen: false });
     }
+
+    legalMove(cardArray, id) {
+        let legalityVerdict = false;
+        if (!cardArray[id].found && !cardArray[id].revealed) {
+            legalityVerdict = true;
+        }
+        return legalityVerdict;
+    }
+
+    turnRevealedCards(cards) {
+        for (let i = 0; i < cards.length; i++) {
+            if (cards[i].revealed && !cards[i].found) {
+                cards[i].revealed = false;
+            }
+        }
+        return cards;
+    }
+
     cardClick(id) {
         let cardArray = this.state.cardStates; // Tillfällig array med objekt för varje korts olika värden
-        if (nrRevealed === 2) {
-            // Vänd ned korten vid klick om två kort redan är uppe
-            let shouldBeOpened = false; // Flagga för om ett klickat kort skall öppnas
-            cardArray.forEach((element, index) => {
-                if (!element.found) {
-                    if (!element.revealed && index === id) {
-                        shouldBeOpened = true;
-                    } else {
-                        element.revealed = false; // Sätt vänd-status till nedvänd på kort som inte ingår i hittade par
-                    }
-                }
-            });
-            if (shouldBeOpened) {
-                cardArray[id].revealed = true;
-                nrRevealed = 1;
-                lookingForPair = cardArray[id].pairID;
-            } else {
-                nrRevealed = 0; // Uppdatera räknaren för antal uppvända kort.
-            }
-            this.setState({ cardStates: cardArray }); // Uppdatera state med data från tillfälig array
-        } else if (nrRevealed === 0 && !cardArray[id].found) {
-            cardArray[id].revealed = !cardArray[id].revealed; // Toggla statusen för uppvänt på rätt objekt i arrayen
-            this.setState({ cardStates: cardArray }); // Uppdatera states med data från den tillfälliga arrayen
-            cardArray[id].revealed ? (nrRevealed += 1) : (nrRevealed -= 1); // Öka eller minska räknare på antal uppvända kort
-            lookingForPair = cardArray[id].pairID;
-        } else if (nrRevealed === 1 && !cardArray[id].revealed) {
-            cardArray[id].revealed = !cardArray[id].revealed; // Toggla statusen för uppvänt på rätt objekt i arrayen
-            cardArray[id].revealed ? (nrRevealed += 1) : (nrRevealed -= 1); // Öka eller minska räknare på antal uppvända kort
+        if (moveAvailableToggle && this.legalMove(cardArray, id)) {
+            cardArray = this.turnRevealedCards(cardArray); // Vänd ned tidigare uppvända kort
+            cardArray[id].revealed = true; // Vänd upp valt kort
+            moveAvailableToggle = false; // Ett av de två dragen är gjort
+            lookingForPair = cardArray[id].pairID; // Vi letar efter ett nytt par
+        } else if (this.legalMove(cardArray, id)) {
+            cardArray[id].revealed = true; // Vänd upp valt kort
+            moveAvailableToggle = true; // Två drag tillgängliga igen
             if (cardArray[id].pairID === lookingForPair) {
-                // Hittat ett par - korten matchar
-                cardArray.forEach((element) => {
-                    // Gå igenom arrayen och markera som hittade
-                    if (element.pairID === lookingForPair) {
-                        element.found = true;
+                // Markera båda korten i paret som hittade
+                for (let i = 0; i < cardArray.length; i++) {
+                    if (cardArray[i].pairID === lookingForPair) {
+                        cardArray[i].found = true;
                     }
-                });
-                foundPairs += 1;
-                if (foundPairs === nrOfPairs) {
-                    this.setState({ solved: true });
                 }
-                nrRevealed = 0;
             }
-            this.setState({ cardStates: cardArray }); // Uppdatera states med data från den tillfälliga arrayen
         }
+        this.setState({ cardStates: cardArray });
     }
 
     render() {
         return (
-            <>	<div className="flex justify-end"><button
-                    onClick={this.toggleMenu}
-                    className="border-2 border-black rounded-lg m-4 p-4 bg-gray-200"
-                >
-                    {this.state.menuOpen ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="feather feather-x"
-                        >
-                            <line x1="18" y1="6" x2="6" y2="18"></line>
-                            <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
-                    ) : (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="feather feather-menu"
-                        >
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
-                        </svg>
-                    )}
-                </button>
-				</div>
+            <>
+                {" "}
+                <div className="flex justify-end">
+                    <button
+                        onClick={this.toggleMenu}
+                        className="border-2 border-black rounded-lg m-4 p-4 bg-gray-200"
+                    >
+                        {this.state.menuOpen ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-x"
+                            >
+                                <line x1="18" y1="6" x2="6" y2="18"></line>
+                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-menu"
+                            >
+                                <line x1="3" y1="12" x2="21" y2="12"></line>
+                                <line x1="3" y1="6" x2="21" y2="6"></line>
+                                <line x1="3" y1="18" x2="21" y2="18"></line>
+                            </svg>
+                        )}
+                    </button>
+                </div>
                 {this.state.menuOpen ? (
                     <div className="flex px-4 flex-wrap justify-center">
                         <button
